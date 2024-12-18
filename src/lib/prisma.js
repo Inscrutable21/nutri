a45b01d263e0db72b1328a2cb844103f4584b9b0
Anand Singh <anandsinghoffical21@gmail.com>
2024-12-18 15:18:20 +0530

    NO

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,13 +1,11 @@
-// lib/prisma.js
 import { PrismaClient } from '@prisma/client'
 
-// Prevent multiple instances of Prisma Client in development
 const globalForPrisma = global
 
-export const prisma = 
+export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['query'],
+    log: ['query', 'error', 'warn'],
   })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
